8efdb8676508c0468fcbf5c8fd71fbee84dd8bd5
Giorgi Charashvili <giorgirock@hotmail.com>
2022-06-21 01:56:38 +0400

    enough!

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ const routes = [
     component: GeometricProggresion,
   },
   {
-    path: '/Math-Project-Website/:pathMatch(.*)*',
+    path: '/:pathMatch(.*)*',
     name: 'Not-Found',
     component: ErrorPage,
   },
