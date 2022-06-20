d8f40fb587f67ebe82c3947617f6a74c80b71a5c
Giorgi Charashvili <giorgirock@hotmail.com>
2022-06-21 03:01:00 +0400

    changed err 404 page

--- src/components/ErrorPage.vue
+++ src/components/ErrorPage.vue
@@ -2,9 +2,7 @@
 
 <template>
   <h1>Opps url page doesn't exist</h1>
-  <router-link class="math-back-btn" to="/Math-Project/">
-    Go Back To Home
-  </router-link>
+  <router-link class="math-back-btn" to="/"> Go Back To Home </router-link>
 </template>
 
 <style scoped></style>
