d9b2349e967b99ae6c5288ab20c5b4dc6e8890a7
Giorgi Charashvili <giorgirock@hotmail.com>
2022-06-21 02:57:25 +0400

    no base

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  base: '/Math-Project/',
   plugins: [vue()],
   build: {
     chunkSizeWarningLimit: 1600,
